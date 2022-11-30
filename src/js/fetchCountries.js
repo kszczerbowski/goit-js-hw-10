e50ffca19bd49c8086559d0eadf5d6d1ec7efdc9
kszczerbowski <kszczerbowski223@gmail.com>
2022-11-30 21:06:38 +0100

    little cleaning

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -4,11 +4,9 @@ import { countryInput } from '../index';
 const countryList = document.querySelector('.country-list');
 const countryDiv = document.querySelector('.country-info');
 
-function cleanHTML() {
+export function fetchCountries(name) {
     countryDiv.innerHTML='';
     countryList.innerHTML = '';
-}
-export function fetchCountries(name) {
     if (countryInput.value !== '') {
         fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
         .then(response => {
@@ -19,16 +17,13 @@ export function fetchCountries(name) {
         })
         .then(data => {
             if (data.length === 1) {
-                cleanHTML();
                 countryDiv.innerHTML = `<img src="${data[0].flags.svg}" alt="flag" width="30">
                 <h1>${data[0].name.official}</h1><br>
                 <h3>Capital: </h3><span>${data[0].capital}</span><br>
                 <h3>Population: </h3><span>${data[0].population}</span><br>
                 <h3>Languages: </h3><span>${Object.values(data[0].languages).join(', ')}</span>`
                 }
-                
             if (data.length >= 2 && data.length <= 10) {
-                cleanHTML();
                 countryList.innerHTML = data.map(element => {
                     return `<li>
                     <img src="${element.flags.svg}" alt="flag" width="40">
@@ -39,10 +34,7 @@ export function fetchCountries(name) {
             if (data.length > 10) Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
         })
         .catch(error => {
-            console.error(error);
             if (error.message == '404') Notiflix.Notify.failure('Oops, there is no country with that name');
         });
-    } else {
-        cleanHTML();
     }
 }
